22d2760f1d934c18624c8bbdf69334761c2bced5
TheKemistLabs <thekemistlabs@gmail.com>
2021-02-02 14:11:45 +0000

    Added layout and activity for adding a new timer.

---
+++ app/src/main/java/com/thecodechemist/countdowntimer/AddTimerActivity.kt
@@ -0,0 +1,11 @@
+package com.thecodechemist.countdowntimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTimerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_timer)
+    }
+}
