03b2cedee6013d8d6f87d684d97cd78b6d4384d4
TheKemistLabs <thekemistlabs@gmail.com>
2021-02-04 15:59:48 +0000

    -Added companion object code to build the Timer database if it does not already exist

--- app/src/main/java/com/thecodechemist/countdowntimer/AppDatabase.kt
+++ app/src/main/java/com/thecodechemist/countdowntimer/AppDatabase.kt
@@ -1,10 +1,33 @@
 package com.thecodechemist.countdowntimer
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = arrayOf(Timer::class), version = 1)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun timerDao(): TimerDao
+
+    abstract fun getTimerDao(): TimerDao
+
+    companion object {
+
+        @Volatile private var instance : AppDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also{
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            "TimerDb"
+        ).build()
+
+    }
+
 
 }
