590e8850ff27c2b72d568e52fe9768f5ed131f1e
TheKemistLabs <thekemistlabs@gmail.com>
2021-02-04 14:26:09 +0000

    -Added override toString method

--- app/src/main/java/com/thecodechemist/countdowntimer/Timer.kt
+++ app/src/main/java/com/thecodechemist/countdowntimer/Timer.kt
@@ -2,9 +2,26 @@ package com.thecodechemist.countdowntimer
 
 import android.os.Parcelable
 import android.util.Log
+import kotlinx.android.parcel.Parcelize
+import java.lang.StringBuilder
 
-class Timer(val name: String, val date: String, val time: String) {
+@Parcelize
+data class Timer(
+        val name: String,
+        val date: String,
+        val time: String): Parcelable {
 
 
+        override fun toString(): String {
+                val sb: StringBuilder = StringBuilder()
+                sb.append("Event Name: ")
+                sb.append(name)
+                sb.append(" | Event Date: ")
+                sb.append(date)
+                sb.append(" | Event Time: ")
+                sb.append(time)
+
+                return sb.toString()
+        }
 }
 
