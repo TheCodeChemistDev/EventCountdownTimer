b52b1f5862800a070b9a1d29b89275eae5f26658
TheKemistLabs <thekemistlabs@gmail.com>
2021-02-04 14:32:07 +0000

    -Added ArrayList to hold Timer objects -Completed function to receive new Timer in onActivityResult and add to ArrayList

--- app/src/main/java/com/thecodechemist/countdowntimer/MainActivity.kt
+++ app/src/main/java/com/thecodechemist/countdowntimer/MainActivity.kt
@@ -3,12 +3,27 @@ package com.thecodechemist.countdowntimer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
+    var timerList = ArrayList<Timer>()
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 1) {
+
+            val timer: Timer = data?.getParcelableExtra("NewTimer")!!
+            if (timer != null) {
+                timerList.add(timer)
+            } else {
+                Log.e("onActivityResult Error", "Received null for timer")
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
