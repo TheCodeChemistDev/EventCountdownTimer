b1c6db8f634e3e8bd8a7228cfbb55289c45ae0c5
TheKemistLabs <thekemistlabs@gmail.com>
2021-02-04 16:33:44 +0000

    -Removed code for starting Activity for Result and onActivityResult override methods. These are no longer needed due to using Room

--- app/src/main/java/com/thecodechemist/countdowntimer/MainActivity.kt
+++ app/src/main/java/com/thecodechemist/countdowntimer/MainActivity.kt
@@ -6,24 +6,12 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.room.Room
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     var timerList = ArrayList<Timer>()
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 1) {
-
-            val timer: Timer = data?.getParcelableExtra("NewTimer")!!
-            if (timer != null) {
-                timerList.add(timer)
-            } else {
-                Log.e("onActivityResult Error", "Received null for timer")
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,7 +32,7 @@ class MainActivity : AppCompatActivity() {
 
     fun goToAddTimer() {
         val intent = Intent(this, AddTimerActivity::class.java)
-        startActivityForResult(intent, 1);
+        startActivity(intent)
     }
     
 }
