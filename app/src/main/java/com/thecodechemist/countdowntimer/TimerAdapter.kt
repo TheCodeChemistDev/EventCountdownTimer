5ce6b27f07b08acc93b5b5d9adbf33fb4d6c6baa
TheKemistLabs <thekemistlabs@gmail.com>
2021-02-08 16:05:51 +0000

    -Fixed code to ensure seconds remaining is shown correctly, previous version always showed remaining seconds as 0.

--- app/src/main/java/com/thecodechemist/countdowntimer/TimerAdapter.kt
+++ app/src/main/java/com/thecodechemist/countdowntimer/TimerAdapter.kt
@@ -33,8 +33,8 @@ class TimerAdapter(private val timers: List<Timer>) : RecyclerView.Adapter<Timer
         sb.append(timers[position].time)
         holder.view.tvEventDetails.text = sb.toString()
 
-        //Set the time remaining
-        val sdf: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy hh:mm")
+        //Set the time remaining...
+        val sdf: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy hh:mm:ss")
 
         //Event Time
         val day: String = timers[position].date.substring(0, 2)
@@ -42,7 +42,7 @@ class TimerAdapter(private val timers: List<Timer>) : RecyclerView.Adapter<Timer
         val year: String = timers[position].date.substring(6, 10)
         val hour: String = timers[position].time.substring(0, 2)
         val minute: String = timers[position].time.substring(3, 5)
-        val eventDateTime = sdf.parse(day + "/" + month + "/" + year + " " + hour + ":" + minute)
+        val eventDateTime = sdf.parse(day + "/" + month + "/" + year + " " + hour + ":" + minute + ":00")
 
         //Current Time
         val now = LocalDateTime.now()
@@ -51,25 +51,23 @@ class TimerAdapter(private val timers: List<Timer>) : RecyclerView.Adapter<Timer
         val currentYear = now.year
         val currentHour = now.hour
         val currentMinute = now.minute
-        val currentDateTime = sdf.parse(Integer.toString(currentDay) + "/" + currentMonth + "/" + currentYear + " " + currentHour + ":" + currentMinute)
+        val currentSecond = now.second
+        val currentDateTime = sdf.parse(Integer.toString(currentDay) + "/" + currentMonth + "/" + currentYear + " " + currentHour + ":" + currentMinute + ":" + currentSecond)
 
         //Calculate the difference
-        // Milli / 1000 = Seconds
-        // 60 Seconds in a Min
-        // 3600 Seconds in an Hour
-        // 86400 Seconds in a Day
-
         var timeDiffInMillis = Math.abs(eventDateTime.time - currentDateTime.time)
         var timeDiffInSecs = timeDiffInMillis / 1000
-        val daysDiff = timeDiffInSecs / 86400
-        val hoursDiff = (timeDiffInSecs % 86400) / 3600
-        val minutesDiff = ((timeDiffInSecs % 86400) % 3600) / 60
-        val secondsDiff = ((timeDiffInSecs % 86400) % 3600) % 60
+        val daysDiff = (timeDiffInMillis / 1000) / 86400
+        val hoursDiff = ((timeDiffInMillis / 1000) % 86400) / 3600
+        val minutesDiff = (((timeDiffInMillis / 1000) % 86400) % 3600) / 60
+        val secondsDiff = (((timeDiffInMillis / 1000) % 86400) % 3600) % 60
         Log.e("Total Time Diff in Secs", "" + timeDiffInSecs)
         Log.e("Diff in Days", "" + daysDiff)
         Log.e("Diff in Hours", "" + hoursDiff)
         Log.e("Diff in Minutes", "" + minutesDiff)
         Log.e("Diff in Secs", "" + secondsDiff)
+
+        //Dislay Time Remaining
         val sb2: StringBuilder = StringBuilder()
         sb2.append(daysDiff)
         sb2.append(" Days, ")
